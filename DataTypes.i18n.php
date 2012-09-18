0965818474d0f87e69a419fe27fac923a4a34113
jeroendedauw <jeroendedauw@gmail.com>
2012-09-18 12:18:37 +0200

    added i18n file and entry point docs

--- DataTypes.i18n.php
+++ DataTypes.i18n.php
@@ -1,8 +1,37 @@
 <?php
+
 /**
- * Created by JetBrains PhpStorm.
- * User: j
- * Date: 9/18/12
- * Time: 12:10 PM
- * To change this template use File | Settings | File Templates.
+ * Internationalization file for the DataTypes extension.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @since 0.1
+ *
+ * @file
+ * @ingroup DataTypes
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  */
+
+$messages = array();
+
+/** English
+ * @author Jeroen De Dauw
+ */
+$messages['en'] = array(
+	'datatypes-desc' => 'Collection of data type definitions',
+);
--- DataTypes.php
+++ DataTypes.php
@@ -1,5 +1,46 @@
 <?php
 
+/**
+ * Entry point for the DataTypes extension.
+ * For usage as MediaWiki extension, use the DataTypes.mw.php entry point.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @since 0.1
+ *
+ * @file
+ * @ingroup DataTypes
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+
+/**
+ * Files belonging to the DataTypes extension.
+ *
+ * @defgroup DataTypes DataTypes
+ */
+
+/**
+ * Tests part of the DataTypes extension.
+ *
+ * @defgroup DataTypesTests DataTypesTests
+ * @ingroup DataTypes
+ */
+
 if ( !defined( 'DATAVALUES' ) ) {
 	die( 'Not an entry point.' );
 }
@@ -7,15 +48,15 @@ if ( !defined( 'DATAVALUES' ) ) {
 define( 'DataTypes_VERSION', '0.1' );
 
 $wgDataTypes = array(
-	'geo' => array(
-		'datavalue' => 'geo-dv',
-		'parser' => 'geo-parser',
-		'formatter' => 'geo-formatter',
-	),
-	'positive-number' => array(
-		'datavalue' => 'numeric-dv',
-		'parser' => 'numeric-parser',
-		'formatter' => 'numeric-formatter',
-		'validators' => array( $rangeValidator ),
-	),
+//	'geo' => array(
+//		'datavalue' => 'geo-dv',
+//		'parser' => 'geo-parser',
+//		'formatter' => 'geo-formatter',
+//	),
+//	'positive-number' => array(
+//		'datavalue' => 'numeric-dv',
+//		'parser' => 'numeric-parser',
+//		'formatter' => 'numeric-formatter',
+//		'validators' => array( $rangeValidator ),
+//	),
 );
