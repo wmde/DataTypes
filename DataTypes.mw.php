24a53d99eaa97a6a545635d42a91b658f4fa0c6e
jeroendedauw <jeroendedauw@gmail.com>
2013-06-07 18:56:35 +0200

    Automatically register tests with MW
    Change-Id: If21ff4ee7178efd10f25d1d166ae879f68191449

--- DataTypes.mw.php
+++ DataTypes.mw.php
@@ -70,14 +70,15 @@ foreach ( include( __DIR__ . '/DataTypes.classes.php' ) as $class => $file ) {
  */
 $wgHooks['UnitTestsList'][] = function( array &$files ) {
 	// @codeCoverageIgnoreStart
-	$testFiles = array(
-		'includes/DataType',
+	$directoryIterator = new \RecursiveDirectoryIterator( __DIR__ . '/tests/phpunit/' );
 
-		'includes/DataTypeFactory',
-	);
-
-	foreach ( $testFiles as $file ) {
-		$files[] = __DIR__ . '/tests/' . $file . 'Test.php';
+	/**
+	 * @var \SplFileInfo $fileInfo
+	 */
+	foreach ( new \RecursiveIteratorIterator( $directoryIterator ) as $fileInfo ) {
+		if ( substr( $fileInfo->getFilename(), -8 ) === 'Test.php' ) {
+			$files[] = $fileInfo->getPathname();
+		}
 	}
 
 	return true;
