9137e60389351fcfa4c22098e919e4bacc7ff886
jeroendedauw <jeroendedauw@gmail.com>
2013-12-23 09:52:37 +0100

    Handle different install locations resource path wise

--- DataTypes.resources.php
+++ DataTypes.resources.php
@@ -13,7 +13,7 @@ return call_user_func( function() {
 
 	$moduleTemplate = array(
 		'localBasePath' => __DIR__ . '/resources',
-		'remoteExtPath' => '../vendor/data-values/data-types/resources',
+		'remoteExtPath' => '..' . substr( __DIR__, strlen( $GLOBALS['IP'] ) ) . '/resources',
 	);
 
 	return array(
