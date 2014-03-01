558969de972f359ff4e7f6da57a4dce2c9a94de5
jeroendedauw <jeroendedauw@gmail.com>
2014-03-02 00:56:55 +0100

    Improve PHPUnit bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,7 +9,14 @@
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  */
 
-echo exec( 'composer update' ) . "\n";
+if ( php_sapi_name() !== 'cli' ) {
+	die( 'Not an entry point' );
+}
+
+$pwd = getcwd();
+chdir( __DIR__ . '/..' );
+passthru( 'composer update' );
+chdir( $pwd );
 
 if ( !is_readable( __DIR__ . '/../vendor/autoload.php' ) ) {
 	die( 'You need to install this package with Composer before you can run the tests' );
