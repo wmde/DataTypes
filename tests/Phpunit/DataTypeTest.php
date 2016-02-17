b326cd81d44207228349cf2e0a677601a447b89b
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2016-02-17 14:21:24 +0100

    Add missing DataTypeTest::tearDown

--- tests/Phpunit/DataTypeTest.php
+++ tests/Phpunit/DataTypeTest.php
@@ -5,6 +5,7 @@ namespace DataTypes\Tests\Phpunit;
 use DataTypes\DataType;
 use DataTypes\Message;
 use PHPUnit_Framework_TestCase;
+use ReflectionClass;
 
 /**
  * @covers DataTypes\DataType
@@ -17,12 +18,27 @@ use PHPUnit_Framework_TestCase;
  */
 class DataTypeTest extends PHPUnit_Framework_TestCase {
 
+	/**
+	 * @var callable|null
+	 */
+	private $textFunction;
+
 	protected function setUp() {
 		parent::setUp();
+
+		$class = new ReflectionClass( 'DataTypes\Message' );
+		$properties = $class->getStaticProperties();
+		$this->textFunction = $properties['textFunction'];
 
 		Message::registerTextFunction( function( $key, $languageCode ) {
 			return implode( '|', func_get_args() );
 		} );
+	}
+
+	protected function tearDown() {
+		Message::registerTextFunction( $this->textFunction );
+
+		parent::tearDown();
 	}
 
 	/**
