f98b7a00c4e66c7823a213ef69dcab09d52d2de5
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2016-12-28 16:00:13 +0100

    Release 1.0.0

--- DataTypes.php
+++ DataTypes.php
@@ -14,7 +14,7 @@ if ( defined( 'DataTypes_VERSION' ) ) {
 	return 1;
 }
 
-define( 'DataTypes_VERSION', '0.5.2' );
+define( 'DataTypes_VERSION', '1.0.0' );
 
 if ( defined( 'MEDIAWIKI' ) ) {
 	include __DIR__ . '/DataTypes.mw.php';
