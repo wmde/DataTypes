383c48c338ef2f3770f36b64093364bb90098ac9
Leszek Manicki <leszek.manicki@wikimedia.de>
2017-07-31 10:10:27 +0200

    Fix indentation of JS block comments

--- js/dataTypes/DataType.js
+++ js/dataTypes/DataType.js
@@ -2,21 +2,21 @@
 	'use strict';
 
 	/**
- * Base constructor for objects representing a data type.
- * @class dataTypes.DataType
- * @abstract
- * @since 0.1
- * @license GPL-2.0+
- * @author Daniel Werner
- * @author H. Snater < mediawiki@snater.com >
- *
- * @constructor
- * @param {string} dataTypeId
- * @param {string} dataValueType
- *
- * @throws {Error} if data type id is not provided as a string.
- * @throws {Error} if data value type is not provided as a string.
- */
+	 * Base constructor for objects representing a data type.
+	 * @class dataTypes.DataType
+	 * @abstract
+	 * @since 0.1
+	 * @license GPL-2.0+
+	 * @author Daniel Werner
+	 * @author H. Snater < mediawiki@snater.com >
+	 *
+	 * @constructor
+	 * @param {string} dataTypeId
+	 * @param {string} dataValueType
+	 *
+	 * @throws {Error} if data type id is not provided as a string.
+	 * @throws {Error} if data value type is not provided as a string.
+	 */
 	var SELF = dt.DataType = function DtDataType( dataTypeId, dataValueType ) {
 		if ( !dataTypeId || typeof dataTypeId !== 'string' ) {
 			throw new Error( 'A data type\'s ID has to be a string' );
@@ -31,47 +31,47 @@
 	};
 
 	$.extend( SELF.prototype, {
-	/**
-	 * DataType identifier.
-	 * @property {string}
-	 * @private
-	 */
+		/**
+		 * DataType identifier.
+		 * @property {string}
+		 * @private
+		 */
 		_id: null,
 
 		/**
-	 * DataValue identifier.
-	 * @property {string}
-	 * @private
-	 */
+		 * DataValue identifier.
+		 * @property {string}
+		 * @private
+		 */
 		_dataValueType: null,
 
 		/**
-	 * Returns the data type's identifier.
-	 *
-	 * @return {string}
-	 */
+		 * Returns the data type's identifier.
+		 *
+		 * @return {string}
+		 */
 		getId: function () {
 			return this._id;
 		},
 
 		/**
-	 * Returns the DataValue used by this data type.
-	 *
-	 * @return {string}
-	 */
+		 * Returns the DataValue used by this data type.
+		 *
+		 * @return {string}
+		 */
 		getDataValueType: function () {
 			return this._dataValueType;
 		}
 	} );
 
 	/**
- * Creates a new DataType object from a given JSON structure.
- * @static
- *
- * @param {string} dataTypeId
- * @param {Object} json
- * @return {dataTypes.DataType}
- */
+	 * Creates a new DataType object from a given JSON structure.
+	 * @static
+	 *
+	 * @param {string} dataTypeId
+	 * @param {Object} json
+	 * @return {dataTypes.DataType}
+	 */
 	SELF.newFromJSON = function ( dataTypeId, json ) {
 		return new SELF( dataTypeId, json.dataValueType );
 	};
--- js/dataTypes/DataTypeStore.js
+++ js/dataTypes/DataTypeStore.js
@@ -2,34 +2,34 @@
 	'use strict';
 
 	/**
- * DataType store.
- * @class dataTypes.DataTypeStore
- * @since 0.2
- * @license GPL-2.0+
- * @author H. Snater < mediawiki@snater.com >
- *
- * @constructor
- */
+	 * DataType store.
+	 * @class dataTypes.DataTypeStore
+	 * @since 0.2
+	 * @license GPL-2.0+
+	 * @author H. Snater < mediawiki@snater.com >
+	 *
+	 * @constructor
+	 */
 	var SELF = dt.DataTypeStore = function DtDataTypeStore() {
 		this._dataTypes = {};
 	};
 
 	$.extend( SELF.prototype, {
-	/**
-	 * Data type definitions.
-	 * @property {Object} [_dataTypes={}]
-	 * @private
-	 */
+		/**
+		 * Data type definitions.
+		 * @property {Object} [_dataTypes={}]
+		 * @private
+		 */
 		_dataTypes: null,
 
 		/**
-	 * Returns the data type of a specific data type id.
-	 *
-	 * @param {string} dataTypeId
-	 * @return {dataTypes.DataType|null}
-	 *
-	 * @throws {Error} when supplied data type id is not a string.
-	 */
+		 * Returns the data type of a specific data type id.
+		 *
+		 * @param {string} dataTypeId
+		 * @return {dataTypes.DataType|null}
+		 *
+		 * @throws {Error} when supplied data type id is not a string.
+		 */
 		getDataType: function ( dataTypeId ) {
 			if ( !dataTypeId || typeof dataTypeId !== 'string' ) {
 				throw new Error( 'The ID given to identify a data type needs to be a string' );
@@ -38,23 +38,23 @@
 		},
 
 		/**
-	 * Returns if there is a DataType of the provided type.
-	 *
-	 * @param {string} dataTypeId
-	 * @return {boolean}
-	 */
+		 * Returns if there is a DataType of the provided type.
+		 *
+		 * @param {string} dataTypeId
+		 * @return {boolean}
+		 */
 		hasDataType: function ( dataTypeId ) {
 			return this._dataTypes[ dataTypeId ] !== undefined;
 		},
 
 		/**
-	 * Registers a new data type. A data type already registered for the id of the new data type
-	 * will be overwritten.
-	 *
-	 * @param {dataTypes.DataType} dataType
-	 *
-	 * @throws {Error} if data type is not a dt.DataType instance.
-	 */
+		 * Registers a new data type. A data type already registered for the id of the new data type
+		 * will be overwritten.
+		 *
+		 * @param {dataTypes.DataType} dataType
+		 *
+		 * @throws {Error} if data type is not a dt.DataType instance.
+		 */
 		registerDataType: function ( dataType ) {
 			if ( !( dataType instanceof dataTypes.DataType ) ) {
 				throw new Error( 'Can only register instances of dataTypes.DataType' );
