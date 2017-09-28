02c5747412e7d00bf66b8bcc576ed9d45b901eaa
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2017-09-28 20:00:10 +0200

    Remove MediaWiki wgExtensionCredits registration

--- DataTypes.mw.php
+++ DataTypes.mw.php
@@ -7,31 +7,15 @@
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  */
 
-namespace DataTypes;
-
 if ( !defined( 'MEDIAWIKI' ) ) {
 	die( 'Not an entry point.' );
 }
-
-$GLOBALS['wgExtensionCredits']['datavalues'][] = [
-	'path' => __DIR__,
-	'name' => 'DataTypes',
-	'version' => DataTypes_VERSION,
-	'author' => [
-		'The Wikidata team',
-	],
-	'url' => 'https://github.com/wmde/DataTypes',
-	'descriptionmsg' => 'datatypes-desc',
-	'license-name' => 'GPL-2.0+'
-];
 
 $GLOBALS['wgMessagesDirs']['DataTypes'] = __DIR__ . '/i18n';
 
 $GLOBALS['wgHooks']['UnitTestsList'][] = function( array &$paths ) {
 	$paths[] = __DIR__ . '/tests/Modules/';
 	$paths[] = __DIR__ . '/tests/Phpunit/';
-
-	return true;
 };
 
 // Resource Loader module registration
